f391eeee282bcafa67a2e71fd6abd8d83ee7456a
rahul dholakiya <dholakiyarahul0@gmail.com>
2024-05-14 17:57:22 +0530

    admin start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./Components/Navbar/Navbar"
+import Sidebar from "./Components/Sidebar/Sidebar"
+import { Route, Routes } from 'react-router-dom';
+import Add from './Pages/Add/Add';
+import Orders from './Pages/Orders/Orders';
+import List from './Pages/List/List';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar/>
+      <hr/>
+      <div className='app-content'>
+      <Sidebar/>
+      <Routes>
+        <Route path='/add' element={<Add/>}/>
+        <Route path='/list' element={<List/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+      </Routes>
+      </div>
     </div>
   );
 }
