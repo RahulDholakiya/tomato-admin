cd2df777ff0e821ed6f98de6edfb3ec932ee7e04
rahul dholakiya <dholakiyarahul0@gmail.com>
2024-05-21 14:25:56 +0530

    admin complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import './App.css';
-import Navbar from "./Components/Navbar/Navbar"
-import Sidebar from "./Components/Sidebar/Sidebar"
-import { Route, Routes } from 'react-router-dom';
-import Add from './Pages/Add/Add';
-import Orders from './Pages/Orders/Orders';
-import List from './Pages/List/List';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./Pages/Add/Add";
+import Orders from "./Pages/Orders/Orders";
+import List from "./Pages/List/List";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const url = "http://localhost:4000";
   return (
     <div>
-      <Navbar/>
-      <hr/>
-      <div className='app-content'>
-      <Sidebar/>
-      <Routes>
-        <Route path='/add' element={<Add/>}/>
-        <Route path='/list' element={<List/>}/>
-        <Route path='/orders' element={<Orders/>}/>
-      </Routes>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
+        </Routes>
       </div>
     </div>
   );
